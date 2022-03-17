a3e6e1d80bce0ec76296ee84deec108270a093f9
Dhananjay Hegde <dananjay123@gmail.com>
2022-03-17 08:24:53 +0530

    change contract address

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,6 @@
 // export const WHITELIST_CONTRACT_ADDRESS = "0x2A5fD78AC27c6d9f7298A3BA4e5F55B2B751eEF0";  // local contract
-export const WHITELIST_CONTRACT_ADDRESS = "0x17F31CD5540E972543e3F113F4c5aA0808F36490";     // rinkeby net contract
+// export const WHITELIST_CONTRACT_ADDRESS = "0x17F31CD5540E972543e3F113F4c5aA0808F36490";     // rinkeby net contract
+export const WHITELIST_CONTRACT_ADDRESS = "0x28c7e07b3df497DD7F3aea9f8a3A427B9CE83e1D" // new one with correct variable names
 export const abi = [
     {
         "inputs": [
